0eee15ef9f267af5453aabcfae2af7e7e219d255
Oxana Michkasova <oxana.mitch@gmail.com>
2024-08-27 18:42:39 -0400

    Member controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
                  registrations: 'users/registrations'
              }
   get '/test', to: 'test#show'
+
+  namespace :api do
+    namespace :v1 do
+      resources :members do
+        resources :facts
+      end
+    end
+  end
 end
